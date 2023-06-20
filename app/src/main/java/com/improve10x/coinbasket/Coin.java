d45d254587fb2cdaad05c3b7556ec747ad75dd65
LENOVO <lakshmi.sannithi@improve10x.com>
2023-06-20 14:52:10 +0530

    I crated new project

---
+++ app/src/main/java/com/improve10x/coinbasket/Coin.java
@@ -0,0 +1,11 @@
+package com.improve10x.coinbasket;
+
+public class Coin {
+    private String id;
+    private String name;
+    private String symbol;
+    private Integer rank;
+    private Boolean isNew;
+    private Boolean isActive;
+    private String type;
+}
